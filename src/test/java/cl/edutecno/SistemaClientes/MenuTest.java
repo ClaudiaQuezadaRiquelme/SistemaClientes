7f4548f4711fbdcee20b73d99119af0f9256a39e
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-06-27 21:43:15 -0400

    MenuTest plantilla

---
+++ src/test/java/cl/edutecno/SistemaClientes/MenuTest.java
@@ -0,0 +1,24 @@
+package cl.edutecno.SistemaClientes;
+
+public class MenuTest {
+	private Menu menu = new Menu();
+	
+	public void testListar() {
+		
+	}
+	public void testAgregar() {
+		
+	}
+	public void testEditar() {
+		
+	}
+	public void testCargar() {
+		
+	}
+	public void testExportar() {
+		
+	}
+	public void testSalir() {
+		
+	}
+}
