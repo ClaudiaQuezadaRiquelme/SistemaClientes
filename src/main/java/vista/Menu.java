6594e1fc0fcf693e242c1d3c567752387a0a781b
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-06-27 22:45:35 -0400

    inicializar atributos clase Menu

--- src/main/java/vista/Menu.java
+++ src/main/java/vista/Menu.java
@@ -1,6 +1,19 @@
 package vista;
 
+import java.util.Scanner;
+
 public class Menu {
+	ClienteServicio clienteServicio;
+	ArchivoServicio archivoServicio;
+	ExportarCsv exportarCsv;
+	ExportarTxt exportarTxt;
+	String fileName = "Clientes";
+	String fileName1 = "DBClientes.csv";
+	Scanner scn;
+	
+	public void iniciarMenu() {
+		
+	}
 
 	public void listar() {
 		
