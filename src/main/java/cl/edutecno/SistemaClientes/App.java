1548705a1e1844d48395f6fb747b595fb8ba7cb7
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-06-27 20:15:30 -0400

    first commit

---
+++ src/main/java/cl/edutecno/SistemaClientes/App.java
@@ -0,0 +1,13 @@
+package cl.edutecno.SistemaClientes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cl/edutecno/SistemaClientes/AppTest.java
@@ -0,0 +1,20 @@
+package cl.edutecno.SistemaClientes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
