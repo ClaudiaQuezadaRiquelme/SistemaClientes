a61604c02139e2a6e229a2bd1175830cd1f10445
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-07-01 02:02:51 -0400

    Testing clienteServicio.editarCliente(cliente, anios)

--- src/test/java/cl/edutecno/SistemaClientes/ClienteServicioTest.java
+++ src/test/java/cl/edutecno/SistemaClientes/ClienteServicioTest.java
@@ -12,9 +12,13 @@ import modelo.Cliente;
 import servicio.ClienteServicio;
 
 public class ClienteServicioTest {
-	private Cliente cliente;
 	private ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
 	private ClienteServicio clienteServicio;
+	private String run = "16.777.888-9";
+	private String nombre = "Pedro";
+	private String apellido = "Picapiedra";
+	private int anios = 54;
+	private CategoriaEnum nombreCategoria = CategoriaEnum.ACTIVO;
 	
 	@Before
 	public void setUp () {
@@ -23,14 +27,19 @@ public class ClienteServicioTest {
 	
 	@Test
 	public void agregarClienteTest () {
-		String run = "16.777.888-9";
-		String nombre = "Pedro";
-		String apellido = "Picapiedra";
-		int anios = 54;
-		CategoriaEnum nombreCategoria = CategoriaEnum.ACTIVO;
 		clienteServicio.agregarCliente(run, nombre, apellido, anios, nombreCategoria);
 		
 		ArrayList<Cliente> testListaClientes = new ArrayList<Cliente> (clienteServicio.getListaCliente());
 		assertEquals(testListaClientes.get(0).getRunCliente(), run);
+	}
+	
+	@Test
+	public void editarClienteAniosTest () {
+		int newAnios = 55;
+		clienteServicio.agregarCliente(run, nombre, apellido, anios, nombreCategoria);
+		
+		Cliente clienteTest = clienteServicio.getListaCliente().get(0);
+		clienteServicio.editarCliente(clienteTest, newAnios);
+		assertEquals(clienteServicio.getListaCliente().get(0).getAniosCliente(), newAnios);
 	}
 }
